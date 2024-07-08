c866bdb1e48883f30664db547cf52011c0143a43
Indu Gundam <indugundam2004@gmail.com>
2024-07-08 21:47:15 +0530

    Adding new file SemesterResults
    Signed-off-by: Indu Gundam <indugundam2004@gmail.com>

--- src/SemesterResults.java
+++ src/SemesterResults.java
@@ -1,25 +1,33 @@
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 public class SemesterResults {
     public static void main(String[] args) throws IOException {
         List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\indug\\OneDrive\\Documents\\Marks.csv"));
-        ArrayList<Integer> total = new ArrayList<>();
-        int totalMarks = 0;
-        for (String line : lines) {
-            String[] marks = line.split(",");
-            for(int i = 0; i < marks.length; i++) {
-                int mark = Integer.parseInt(marks[i]);
-                totalMarks += mark;
-            }
-            total.add(totalMarks);
+        String fileName = "C:\\Users\\indug\\OneDrive\\Documents\\semMarks.csv";
+        FileWriter writer = new FileWriter(fileName);
+        int totalMarks;
+        int highest = Integer.MIN_VALUE;
+        int semester = 0;
+        for (int i = 0; i < lines.size(); i++) {
             totalMarks = 0;
+            String[] marks = lines.get(i).split(",");
+            for (int j = 0; j < marks.length; j++) {
+                int mark = Integer.parseInt(marks[j]);
+                totalMarks += mark;
+                if (totalMarks > highest) {
+                    highest = totalMarks;
+                    semester = i + 1;
+                }
+            }
+            writer.write(lines.get(i) + "," + totalMarks + "\n");
         }
-        for(int ele : total) {
-            System.out.println(ele);
-        }
+        writer.flush();
+        System.out.println("Best marks are scored in semester " + semester);
     }
+
 }
+
